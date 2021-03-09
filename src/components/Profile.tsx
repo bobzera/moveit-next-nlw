27dd19a37e0b921ebf9b741c4743c636957e629b
bobzera <weslleyzera2020@gmail.com>
2021-03-09 13:17:12 +0000

    back

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/bobzera.png" alt="Weslley Oliveira"/>
+      <img src="https://github.com/weslley-oliveira.png" alt="Weslley Oliveira"/>
       <div>
         <strong>Weslley Oliveira</strong>
         
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
