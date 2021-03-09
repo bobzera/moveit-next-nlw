f4900dfb206ddc151115b702c5c508aed74fe74a
bobzera <weslleyzera2020@gmail.com>
2021-03-09 20:14:41 +0000

    teste

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -70,11 +70,11 @@ export function ChallengesProvider({ children, ...rest }: ChallengesProviderProp
 
   //  new Audio('/notification.mp3').play();
 
-    if(Notification.permission === 'granted') {
-      new Notification('Novo desafio 🎉', { 
-        body: `Valendo ${challenge.amount}xp!`
-      })
-    }
+    // if(Notification.permission === 'granted') {
+    //   new Notification('Novo desafio 🎉', { 
+    //     body: `Valendo ${challenge.amount}xp!`
+    //   })
+    // }
   }
 
   function resetChallenge() {
